e43f053c927b2a008f15ec6d7929506465c48451
Leone Lima <leonnylimas@gmail.com>
2024-03-06 13:21:21 -0300

    foi adicionado as operações no sistema

--- infixcalc.py
+++ infixcalc.py
@@ -26,3 +26,79 @@ n2: 4
 
 __version__ = "0.1.0"
 
+import sys
+arguments = sys.argv[1:]
+
+if not arguments:
+    operation = input("Operação:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+    print("Número de argumentos inválidos")
+    print("ex: `sum 5 5`")
+    sys.exit(1)
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+
+if operation not in valid_operations:
+    print("Operação inválida")
+    print("Segue ex das operações validas: ", valid_operations)
+    sys.exit(1)
+
+validated_nums = []
+for num in nums:
+    if not num.replace(".","").isdigit():
+        print(f"Numero inválido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+#TODO: Usar dict de funções
+if operation == "sum":
+    result = n1 + n2 
+if operation == "sub":
+    result = n1 - n2
+if operation == "mul":
+    result = n1 * n2
+if operation == "div":
+    result = n1 / n2 
+
+print(f"O resultado é: {result}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
